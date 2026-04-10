ec680d4151753454617cb2681ad2e301d4a79675
Jamie Baddeley <jamie@mixmat.es>
2026-04-10 16:29:20 +1200

    Initial Commit

---
+++ Listener/ListenerApp.swift
@@ -0,0 +1,17 @@
+//
+//  ListenerApp.swift
+//  Listener
+//
+//  Created by jamie baddeley on 10/04/2026.
+//
+
+import SwiftUI
+
+@main
+struct ListenerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
